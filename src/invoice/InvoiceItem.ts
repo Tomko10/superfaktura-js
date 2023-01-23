90b665615ae65bfa0b29f0d308a077ef6fdac8db
Tomáš Jusko <sj.tomi@gmail.com>
2023-01-23 13:02:22 +0100

    feat: Added invoice item parsing

--- src/invoice/InvoiceItem.ts
+++ src/invoice/InvoiceItem.ts
@@ -1,4 +1,6 @@
 import { SFClient } from '../SFClient';
+import { PartialNullable } from '../utils/types';
+import { nullableFloat, nullableInt } from '../utils/parse';
 
 interface IBaseInvoiceItem {
   description: string;
@@ -46,8 +48,38 @@ export default class InvoiceItem {
     this.client = client;
   }
 
-  static parseResponse(data: any): IGetInvoiceItem | null {
+  static parseResponse(data: any): PartialNullable<IGetInvoiceItem> | null {
     // TODO: Implement
-    return null;
+    if (!data) return null;
+    return {
+      description: data.description,
+      discount: parseFloat(data.discount),
+      discount_description: data.discount_description,
+      discount_no_vat: parseFloat(data.discount_no_vat),
+      discount_no_vat_total: parseFloat(data.discount_no_vat_total),
+      discount_with_vat: parseFloat(data.discount_with_vat),
+      discount_with_vat_total: parseFloat(data.discount_with_vat_total),
+      id: parseInt(data.id, 10),
+      invoice_id: parseInt(data.invoice_id, 10),
+      item_price: parseFloat(data.item_price),
+      item_price_no_discount: parseFloat(data.item_price_no_discount),
+      item_price_vat: parseFloat(data.item_price_vat),
+      item_price_vat_check: parseFloat(data.item_price_vat_check),
+      item_price_vat_no_discount: parseFloat(data.item_price_vat_no_discount),
+      name: data.name,
+      ordernum: data.ordernum,
+      quantity: parseFloat(data.quantity),
+      sku: data.sku,
+      stock_item_id: nullableInt(data.stock_item_id),
+      tax: parseFloat(data.tax),
+      tax_deposit: nullableFloat(data.tax_deposit),
+      unit: data.unit,
+      unit_price: parseFloat(data.unit_price),
+      unit_price_discount: parseFloat(data.unit_price_discount),
+      unit_price_vat: parseFloat(data.unit_price_vat),
+      unit_price_vat_no_discount: parseFloat(data.unit_price_vat_no_discount),
+      user_id: data.user_id,
+      user_profile_id: data.user_profile_id,
+    };
   }
 }
